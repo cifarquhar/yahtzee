f75868971c11d0a0bacc9c33206f3ea2a58ee16a
Colin Farquhar <cifarquhar@gmail.com>
2018-05-31 12:41:03 +0100

    add roll method to dice conainer

--- src/containers/DiceContainer.jsx
+++ src/containers/DiceContainer.jsx
@@ -6,7 +6,11 @@ import "../styles/containers.css";
 class DiceContainer extends Component {
 
   constructor(props){
-    super(props)
+    super(props);
+
+    this.state = {
+      diceValue: null
+    };
 
     this.die1 = {value: 6, held: false};
     this.die2 = {value: 3, held: false};
@@ -17,6 +21,31 @@ class DiceContainer extends Component {
     this.dice = [this.die1, this.die2, this.die3, this.die4, this.die5];
 
   }
+
+  
+  getDieValue() {
+    return Math.floor(Math.random() * 6) + 1;
+  }
+
+
+  rollDice(){
+    this.dice.forEach(die => {
+      if (!die.held){
+        die.value = this.getDieValue();
+        console.log(die.value);
+      }
+    });
+
+    let totalValue = 0;
+
+    this.dice.forEach(die => {
+      totalValue += die.value
+    })
+
+    this.setState({diceValue: totalValue});
+
+  }
+
 
   render() {
 
@@ -35,7 +64,7 @@ class DiceContainer extends Component {
         <div className="dice-container">
           {diceToRender}
         </div>
-        <Button block>Roll</Button>
+        <Button onClick={this.rollDice.bind(this)} block>Roll</Button>
       </div>
     );
 
