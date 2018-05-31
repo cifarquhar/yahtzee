cab41fbdcf4ef92442808a41bf6b42de4d670148
Colin Farquhar <cifarquhar@gmail.com>
2018-05-31 12:25:32 +0100

    refactor dice rendering

--- src/containers/DiceContainer.jsx
+++ src/containers/DiceContainer.jsx
@@ -5,16 +5,35 @@ import "../styles/containers.css";
 
 class DiceContainer extends Component {
 
+  constructor(props){
+    super(props)
+
+    this.die1 = {value: 6, held: false};
+    this.die2 = {value: 3, held: false};
+    this.die3 = {value: 2, held: false};
+    this.die4 = {value: 5, held: false};
+    this.die5 = {value: null, held: false};
+
+    this.dice = [this.die1, this.die2, this.die3, this.die4, this.die5];
+
+  }
+
   render() {
+
+    const diceToRender = this.dice.map((array,index) => {
+      return(
+        <Dice
+          key={index}
+          value={this.dice[index].value}
+          held={this.dice[index].held}
+        />
+      )
+    })
 
     return (
       <div>
         <div className="dice-container">
-          <Dice value={1}/>
-          <Dice value={2}/>
-          <Dice value={3}/>
-          <Dice value={4}/>
-          <Dice value={5}/>
+          {diceToRender}
         </div>
         <Button block>Roll</Button>
       </div>
