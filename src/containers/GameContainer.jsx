13c1359d016180fde63db715e1b940ad040975ed
Colin Farquhar <cifarquhar@gmail.com>
2018-05-31 10:40:19 +0100

    add GameContainer

---
+++ src/containers/GameContainer.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+
+class GameContainer extends Component {
+
+  render(){
+
+    return (
+      <div>
+        <p>Content goes here</p>
+      </div>
+    );
+
+  }
+
+}
+
+export default GameContainer;
