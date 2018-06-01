7f619d0ea8adbf5e14a44e1eeb887a9d48dbf97e
Colin Farquhar <cifarquhar@gmail.com>
2018-06-01 14:01:42 +0100

    add scorecard component to card container, modify css

---
+++ src/components/Scorecard.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from "react";
+
+import "../styles/components.css";
+
+class Scorecard extends Component {
+
+  constructor(props){
+    super(props)
+  }
+
+
+  render(){
+    return(
+      <div>
+        <p>Score</p>
+      </div>
+    )
+  }
+
+}
+
+export default Scorecard;
--- src/containers/CardContainer.jsx
+++ src/containers/CardContainer.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
+import Scorecard from "../components/Scorecard";
+import "../styles/containers.css";
 
 class CardContainer extends Component {
 
   render() {
 
     return (
-      <div>
-        <p>Player score information goes in here</p>
+      <div className="card-container">
+        <Scorecard/>
       </div>
     );
 
