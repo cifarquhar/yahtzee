49336fafcaa5ffb90bca379ec7a28f03b0dd6af7
Colin Farquhar <cifarquhar@gmail.com>
2018-06-01 14:17:09 +0100

    add skeleton to scorecard

--- src/components/Scorecard.jsx
+++ src/components/Scorecard.jsx
@@ -12,7 +12,88 @@ class Scorecard extends Component {
   render(){
     return(
       <div>
-        <p>Score</p>
+        <table>
+          <tr>
+            <th>Upper Section</th>
+          </tr>
+          <tr>
+            <td>Aces</td>
+            <td>X</td>
+          </tr>
+          <tr>
+            <td>Twos</td>
+            <td>X</td>
+          </tr>
+          <tr>
+            <td>Threes</td>
+            <td>X</td>
+          </tr>
+          <tr>
+            <td>Fours</td>
+            <td>X</td>
+          </tr>
+          <tr>
+            <td>Fives</td>
+            <td>X</td>
+          </tr>
+          <tr>
+            <td>Sixes</td>
+            <td>X</td>
+          </tr>
+          <tr>
+            <th>Upper Section</th>
+            <td>X</td>
+          </tr>
+          <tr>
+            <th>Upper Section Total</th>
+            <td>X</td>
+          </tr>
+          <tr>
+          </tr>
+          <tr>
+            <th>Lower Section</th>
+          </tr>
+          <tr>
+            <td>Three-of-a-kind</td>
+            <td>X</td>
+          </tr>
+          <tr>
+            <td>Four-of-a-kind</td>
+            <td>X</td>
+          </tr>
+          <tr>
+            <td>Full House</td>
+            <td>X</td>
+          </tr>
+          <tr>
+            <td>Low Straight</td>
+            <td>X</td>
+          </tr>
+          <tr>
+            <td>High Straight</td>
+            <td>X</td>
+          </tr>
+          <tr>
+            <td>Yahtzee</td>
+            <td>X</td>
+          </tr>
+          <tr>
+            <td>Chance</td>
+            <td>X</td>
+          </tr>
+          <tr>
+            <th>Yahtzee Bonus</th>
+            <td>X</td>
+          </tr>
+          <tr>
+            <th>Lower Section Total</th>
+            <td>X</td>
+          </tr>
+          <tr>
+            <th>Grand Total</th>
+            <td>X</td>
+          </tr>
+        </table>
       </div>
     )
   }
